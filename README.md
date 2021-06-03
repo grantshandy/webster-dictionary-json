274f8fb727c0a3ed79b19d4255a3f3dea45125f7
DefunctLizard <grantshandy@gmail.com>
2021-06-03 16:53:11 -0600

    finish basic word processing

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # webster-dictionary-json
-A script to generate a dictionary JSON file.
+A simple rust script to generate a dictionary JSON file from Webster's Unabridged Dictionary with a focus on making the file as small as possible.
+
+## License
+Code:
+> MIT License
+> 
+> Copyright (c) 2021 Grant Handy
+> 
+> Permission is hereby granted, free of charge, to any person obtaining a copy
+> of this software and associated documentation files (the "Software"), to deal
+> in the Software without restriction, including without limitation the rights
+> to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+> copies of the Software, and to permit persons to whom the Software is
+> furnished to do so, subject to the following conditions:
+> 
+>The above copyright notice and this permission notice shall be included in all
+> copies or substantial portions of the Software.
+> 
+> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+> IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+> FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+> AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+> LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+> OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+> SOFTWARE.
+
+From `original_dictionary.txt`:
+> The Project Gutenberg EBook of Webster's Unabridged Dictionary, by Various
+>
+> This eBook is for the use of anyone anywhere at no cost and with
+> almost no restrictions whatsoever.  You may copy it, give it away or
+> re-use it under the terms of the Project Gutenberg License included
+> with this eBook or online at www.gutenberg.net
+>
+>
+> Title: Webster's Unabridged Dictionary
+>
+> Author: Various
+>
+> Release Date: August 22, 2009 [EBook #29765]
+>
+> Language: English
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,59 @@
+use std::collections::HashMap;
+// use serde_json::Value;
+
 const DICTIONARY: &'static str = include_str!("dictionary_original.txt");
 
 fn main() {
     let lines = DICTIONARY.lines();
-    let mut words: Vec<&str> = Vec::new();
+    let mut definition_list: HashMap<String, String> = HashMap::new();
+    let mut current_line: usize = 0;
+    let mut current_letter: char = 'A';
+
+    let bad_characters = vec!["(", ")", "[", "]", ".", "*", ",", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
     'lines: for line in lines {
-        let bad_characters = vec!["(", ")", "[", "]", ".", "*", ",", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+        current_line += 1;
 
         if line != "" {
-            for char in bad_characters {
-                if line.contains(char) || line == char {
+            for char in &bad_characters {
+                if line.contains(char) || &line == char {
                     continue 'lines;
                 };
             };
 
             if line.split(' ').collect::<Vec<&str>>().len() == 1 {
-                if line.to_uppercase() == line {
-                    if !words.contains(&line) {
-                        words.push(line);
-                        println!("{}", line);
+                let word = line;
+                if word.to_uppercase() == word {
+                    if !definition_list.contains_key(&word.to_string()) {
+                        let first_letter = word.chars().nth(0).unwrap();
+
+                        if first_letter != current_letter {
+                            if first_letter == '\'' || first_letter == '-' {
+                                continue 'lines;
+                            } else {
+                                current_letter = first_letter;
+                                println!("Processing letter {}...", current_letter);
+                            }
+                        };
+
+                        let definition = get_definition(word, current_line);
+
+                        definition_list.insert(word.to_string().to_lowercase(), definition);
                     };
                 };
             };
         };
     };
+
+    let query = "stupid";
+
+    match definition_list.get(query) {
+        Some(data) => println!("{} definition: {}", query, data),
+        None => eprintln!("no data for {}", query),
+    }
+
+}
+
+fn get_definition(_word: &str, _line: usize) -> String {
+    String::from("placeholder definition")
 }
