98ee11725f258b992068d35a4e3e008c3a7385ec
grantshandy <grantshandy@gmail.com>
2021-06-06 00:59:32 -0600

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,78 @@
-use std::{collections::HashMap, str::Lines};
+#![feature(const_btree_new)]
+
+use std::{collections::BTreeMap, str::Lines};
 
 const DICTIONARY: &'static str = include_str!("dictionary_original.txt");
+static mut DEFINITION_LIST: BTreeMap<String, String> = BTreeMap::new();
 
 fn main() {
     let lines = DICTIONARY.lines();
     collect(lines);
-    // let definition = get_definition(8137, lines);
+    // let definition = get_definition(7630, lines);
     // println!("\n\ndefinition: {}", definition);
 }
 
-fn get_definition(line: usize, lines: Lines) -> String {
+fn collect(lines: Lines) {
+    let mut current_line: usize = 0;
+    let mut current_letter: char = ' ';
+
+    let bad_characters = vec![
+        "(", ")", "[", "]", ".", "*", ",", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+    ];
+
+    'lines: for line in lines.clone().into_iter() {
+        current_line += 1;
+
+        if line != "" {
+            for char in &bad_characters {
+                if line.contains(char) || &line == char {
+                    continue 'lines;
+                };
+            }
+
+            if line.split(' ').collect::<Vec<&str>>().len() == 1 {
+                let word = line;
+                if word.to_uppercase() == word {
+                    let first_letter = word.chars().nth(0).unwrap();
+
+                    if first_letter != current_letter {
+                        if first_letter == '\'' || first_letter == '-' {
+                            continue 'lines;
+                        } else {
+                            current_letter = first_letter;
+                            println!("Processing letter {}...", current_letter);
+                        }
+                    };
+
+                    let w = word.to_string();
+                    let definition = get_definition(current_line, lines.clone());
+
+                    unsafe {
+                        DEFINITION_LIST.insert(w, definition);
+                    };
+
+                    // println!("{} - {}", word, definition);
+                };
+            };
+        };
+    }
+
+    let query = String::from("stupid");
+
+    unsafe {
+        match DEFINITION_LIST.get(&query) {
+            Some(data) => println!("{} definition: {}", query, data),
+            None => eprintln!("no data for {}", query),
+        };
+    };
+}
+
+fn get_definition<'a>(line: usize, lines: Lines) -> String {
     let lines_orig = lines.clone().skip(line + 1);
     let mut current_line: usize = line;
 
     // iterate through the lines after our requested line
     for x in lines_orig.clone() {
-
         current_line += 1;
         // println!("{}: {}", current_line, x);
 
@@ -32,9 +89,9 @@ fn get_definition(line: usize, lines: Lines) -> String {
         if is_word(&x) {
             panic!("hit a word before a definition... {}, {}", current_line, x);
         };
-    };
+    }
 
-    String::from("Couldn't detect a definition")
+    "Couldn't detect a definition".to_string()
 }
 
 fn check_for_num(x: &str, lines: Lines, current_line: usize) -> Option<String> {
@@ -49,15 +106,15 @@ fn check_for_num(x: &str, lines: Lines, current_line: usize) -> Option<String> {
             if x.chars().nth(0).unwrap() == '(' && x.chars().last().unwrap() == ')' {
                 for y in lines.clone().skip(current_line + 1) {
                     // println!("{}: {}", current_line, y);
-                    
-                    match check_for_defn(y, lines.clone(), (current_line + 2)) {
+
+                    match check_for_defn(y, lines.clone(), current_line + 2) {
                         Some(data) => {
                             // println!("data: {}", data);
                             return Some(data);
-                        },
+                        }
                         None => (),
                     };
-                };
+                }
             };
 
             x = x.split('.').nth(0).unwrap().to_string();
@@ -70,7 +127,7 @@ fn check_for_num(x: &str, lines: Lines, current_line: usize) -> Option<String> {
                 // println!("        {}", y);
 
                 if y.contains('.') {
-                    let y  = y.split('.').nth(0).unwrap().to_string();
+                    let y = y.split('.').nth(0).unwrap().to_string();
 
                     x.push_str(&format!(" {}.", y));
 
@@ -78,10 +135,10 @@ fn check_for_num(x: &str, lines: Lines, current_line: usize) -> Option<String> {
                 } else {
                     x.push_str(&format!(" {}", y));
                 };
-            };
+            }
         };
     };
-    
+
     return None;
 }
 
@@ -90,7 +147,7 @@ fn check_for_defn(x: &str, lines: Lines, current_line: usize) -> Option<String> 
     let mut split = x.split(':');
 
     // if there's a Defn before the :
-    if  split.nth(0).unwrap() == "Defn" {
+    if split.nth(0).unwrap() == "Defn" {
         // println!("found a defn...");
         // remove "Defn: " from the line
         let mut x = (&x[6..x.len()]).to_string();
@@ -113,7 +170,7 @@ fn check_for_defn(x: &str, lines: Lines, current_line: usize) -> Option<String> 
             // if it has no period iterate through the next lines.
             for y in lines.clone().skip(current_line + 1) {
                 if y.contains('.') {
-                    let y  = y.split('.').nth(0).unwrap().to_string();
+                    let y = y.split('.').nth(0).unwrap().to_string();
 
                     // println!("pushing {}", y);
                     x.push_str(&format!(" {}.", y));
@@ -123,22 +180,24 @@ fn check_for_defn(x: &str, lines: Lines, current_line: usize) -> Option<String> 
                     // println!("pushing {}", y);
                     x.push_str(&format!(" {}", y));
                 };
-            };
+            }
         };
     };
 
     return None;
 }
 
-fn is_word(line: &str) -> bool { 
+fn is_word(line: &str) -> bool {
     if line != "" {
-        let bad_characters = vec!["(", ")", "[", "]", ".", "*", ",", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+        let bad_characters = vec![
+            "(", ")", "[", "]", ".", "*", ",", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+        ];
 
         for char in bad_characters {
             if line.contains(char) || line == char {
                 return false;
             };
-        };
+        }
 
         if line.split(' ').collect::<Vec<&str>>().len() == 1 {
             if line.to_uppercase() == line {
@@ -148,58 +207,4 @@ fn is_word(line: &str) -> bool {
     };
 
     return false;
-}
-
-fn collect(lines: Lines) {
-    let mut definition_list: HashMap<String, String> = HashMap::new();
-    let mut current_line: usize = 0;
-    let mut current_letter: char = ' ';
-
-    let bad_characters = vec!["(", ")", "[", "]", ".", "*", ",", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-
-    'lines: for line in lines.clone().into_iter() {
-        current_line += 1;
-
-        if line != "" {
-            for char in &bad_characters {
-                if line.contains(char) || &line == char {
-                    continue 'lines;
-                };
-            };
-
-            if line.split(' ').collect::<Vec<&str>>().len() == 1 {
-                let word = line;
-                if word.to_uppercase() == word {
-                    let first_letter = word.chars().nth(0).unwrap();
-
-                    if first_letter != current_letter {
-                        if first_letter == '\'' || first_letter == '-' {
-                            continue 'lines;
-                        } else {
-                            current_letter = first_letter;
-                            println!("Processing letter {}...", current_letter);
-                        }
-                    };
-
-                    let word = word.to_lowercase();
-
-                    let definition = get_definition(current_line, lines.clone());
-
-                    println!("{} - {}", word, definition);
-
-                    // let definition = get_definition(current_line, lines.clone());
-
-                    // definition_list.insert(word.to_string().to_lowercase(), definition);
-                };
-            };
-        };
-    };
-
-    let query = "stupid";
-
-    match definition_list.get(query) {
-        Some(data) => println!("{} definition: {}", query, data),
-        None => eprintln!("no data for {}", query),
-    }
-
 }
